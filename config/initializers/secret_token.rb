f04ae8e62a3f676dc5bd50485c987eef88bd8cd5
Hiroshi Saito <hiroshi3110@gmail.com>
2012-05-24 19:21:13 +0900

    rails new hubitems -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hubitems::Application.config.secret_token = 'bce4f436d509f5c58170c48fee9fbe053933560184072423eb9147ceedc10d409bc2975b7d52dc2b8ce60ff5b6a8905cb71a6ceb72d1d0c5f28acc8a96646164'
